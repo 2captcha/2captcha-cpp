af607a9f4f3b1ad5cf101eccfa4f95f77810750e
victorkowalski <vkowalski@yandex.ru>
2024-08-06 11:54:34 +0300

    lemin draft

--- examples/lemin.cpp
+++ examples/lemin.cpp
@@ -29,3 +29,16 @@ int main (int ac, char ** av)
 
    return 0;   
 }
+/*
+        Lemin captcha = new Lemin();      
+        captcha.setСaptchaId("CROPPED_d3d4d56_73ca4008925b4f83a8bed59c2dd0df6d");
+        captcha.setUrl("http://sat2.aksigorta.com.tr");
+        captcha.setApiServer("api.leminnow.com");
+
+        try {
+            solver.solve(captcha);
+            System.out.println("Captcha solved: " + captcha.getCode());
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
+        */
