ec9bfee80c6a0fa25e550e6c1ea25de2d2312eb5
victorkowalski <vkowalski@yandex.ru>
2024-08-07 08:34:54 +0300

    setCaptchaid

--- examples/lemin.cpp
+++ examples/lemin.cpp
@@ -13,7 +13,7 @@ int main (int ac, char ** av)
    client.set_api_key (API_KEY);
 
    api2captcha::lemin lemin;
-   lemin.serCaptchaid ("CROPPED_d3d4d56_73ca4008925b4f83a8bed59c2dd0df6d");
+   lemin.setCaptchaid ("CROPPED_d3d4d56_73ca4008925b4f83a8bed59c2dd0df6d");
    lemin.setUrl ("http://sat2.aksigorta.com.tr");
    lemin.setApiServer("api.leminnow.com");
 
